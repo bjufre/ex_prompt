b94004f7b08af3bc433d050f52a4975b68e651ea
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2021-06-11 14:02:51 -0300

    Add default values to string, confirm and choose functions

--- lib/ex_prompt.ex
+++ lib/ex_prompt.ex
@@ -17,29 +17,31 @@ defmodule ExPrompt do
   @doc """
   Reads a line from `:stdio` displaying the prompt that is passed in.
 
-  In case of any errors or `:eof` this function will return and empty string.
+  In case of any errors or `:eof` this function will return the default value if present, or an empty string otherwise.
 
   ## Examples
 
   To ask a user for their name and await for the input:
 
     ExPrompt.string("What is your name?\n")
+
+  To ask for a hostname defaulting to `localhost`:
+
+    ExPrompt.string("What is the hostname?\n", "localhost")
   """
   @spec string(prompt) :: String.t()
-  def string(prompt) do
-    case IO.gets(prompt) do
-      :eof -> ""
-      {:error, _reason} -> ""
-      str -> String.trim_trailing(str)
-    end
+  @spec string(prompt, String.t()) :: String.t()
+  def string(prompt, default \\ "") do
+    string_prompt(prompt, default)
+    |> ask(default)
   end
 
-  @doc "Alias for `string/1`."
-  @spec get(prompt) :: String.t()
-  def get(prompt), do: string(prompt)
+  @doc "Alias for `string/2`."
+  @spec get(prompt, String.t()) :: String.t()
+  def get(prompt, default \\ ""), do: string(prompt, default)
 
   @doc """
-  Same as `string/1` but it will continue to "prompt" the user in case of an empty response.
+  Same as `string/2` but it will continue to "prompt" the user in case of an empty response.
   """
   @spec string_required(prompt) :: String.t()
   def string_required(prompt) do
@@ -59,32 +61,39 @@ defmodule ExPrompt do
     - Yes, yes, YES, Y, y
     - No, no, NO, N, n
 
-  In case that the answer is none of the above, it will prompt again until we do.
+  In case that the answer is none of the above, it will prompt again until we do or the default value if it's present.
 
   ## Examples
 
   To ask whether the user wants to delete a file or not:
 
     ExPrompt.confirm("Are you sure you want to delete this file?")
+
+  It's the same example above, returning false as default.
+
+    ExPrompt.confirm("Are you sure you want to delete this file?", false)
   """
   @spec confirm(prompt) :: boolean()
-  def confirm(prompt) do
+  @spec confirm(prompt, boolean() | nil) :: boolean()
+  def confirm(prompt, default \\ nil) do
     answer =
-      String.trim(prompt)
-      |> Kernel.<>(" [Yn] ")
-      |> string()
+      prompt
+      |> boolean_prompt(default)
+      |> ask()
       |> String.downcase()
 
     cond do
       answer in ~w(yes y) -> true
       answer in ~w(no n) -> false
-      true -> confirm(prompt)
+      answer == "" and default != nil -> default
+      true -> confirm(prompt, default)
     end
   end
 
-  @doc "Alias for `confirm/1`."
+  @doc "Alias for `confirm/2`."
   @spec yes?(prompt) :: boolean()
-  def yes?(prompt), do: confirm(prompt)
+  @spec yes?(prompt, boolean() | nil) :: boolean()
+  def yes?(prompt, default \\ nil), do: confirm(prompt, default)
 
   @doc """
   Asks the user to select form a list of choices.
@@ -96,29 +105,41 @@ defmodule ExPrompt do
   by the value that the user wrote.
 
   ## Examples
+  
+  To ask for a favorite color in a predefined list
 
     ExPrompt.choose("Favorite color?" , ~w(red green blue))
+  
+  It's the same example above, but defines to the second option (green) as default value if none is selected.
+
+    ExPrompt.choose("Favorite color?" , ~w(red green blue), 2)
   """
   @spec choose(prompt, choices) :: integer()
-  def choose(prompt, choices) do
+  @spec choose(prompt, choices, integer()) :: integer()
+  def choose(prompt, choices, default \\ -1) do
     IO.puts("")
 
+    if default < -1 || default >= length(choices) do
+      raise "Invalid default value, the value must be between -1 (none) and #{length(choices) - 1}"
+    end
+
     answer =
-      Enum.with_index(choices)
-      |> Enum.reduce("", fn {c, i}, acc ->
-        "#{acc}\s\s#{i + 1}) #{c}\n"
-      end)
-      |> Kernel.<>("\n" <> String.trim(prompt) <> "\s")
-      |> string()
+      list_prompt(prompt, choices, default)
+      |> ask()
 
     try do
       n = String.to_integer(answer)
       if n > 0 and n <= length(choices), do: n - 1, else: -1
     rescue
       _e in ArgumentError ->
-        case Enum.find_index(choices, &(&1 == answer)) do
-          nil -> -1
-          idx -> idx
+        case answer do
+          "" ->
+            default
+          _ ->
+            case Enum.find_index(choices, &(&1 == answer)) do
+              nil -> -1
+              idx -> idx
+            end
         end
     end
   end
@@ -136,6 +157,7 @@ defmodule ExPrompt do
 
     ExPrompt.password("Password: ", false)
   """
+  @spec password(prompt) :: String.t()
   @spec password(prompt, hide :: boolean()) :: String.t()
   def password(prompt, hide \\ true) do
     prompt = String.trim(prompt)
@@ -168,4 +190,56 @@ defmodule ExPrompt do
         pw_loop(prompt)
     end
   end
+  
+  defp format_trailing(prompt, default \\ nil) do
+    case Regex.named_captures(~r/(?<spaces>[[:space:]]*)$/, prompt) do
+      %{"spaces" => ""} when default != nil ->
+        "#{String.trim_trailing(prompt)} #{default} "
+      %{"spaces" => ""} ->
+        "#{String.trim_trailing(prompt)} "
+      %{"spaces" => spaces} when default != nil ->
+        "#{String.trim_trailing(prompt)} #{default}#{spaces}"
+      %{"spaces" => spaces} ->
+        "#{String.trim_trailing(prompt)}#{spaces}"
+    end
+  end
+  
+  defp format_choices(choices) do
+    choices
+    |> Enum.with_index()
+    |> Enum.reduce("", fn {c, i}, acc ->
+      "#{acc}  #{i + 1}) #{c}\n"
+    end)
+  end
+
+  defp string_prompt(prompt, ""), do: format_trailing(prompt)
+  defp string_prompt(prompt, nil), do: format_trailing(prompt)
+  defp string_prompt(prompt, default), do: format_trailing(prompt, "(#{default})")
+
+  defp boolean_prompt(prompt, nil), do: format_trailing(prompt, "[yn]")
+  defp boolean_prompt(prompt, true), do: format_trailing(prompt, "[Yn]")
+  defp boolean_prompt(prompt, false), do: format_trailing(prompt, "[yN]")
+  
+  defp list_prompt(prompt, choices, -1) do
+    "#{format_choices(choices)}\n#{format_trailing(prompt)}"
+  end
+  
+  defp list_prompt(prompt, choices, default) do
+    "#{format_choices(choices)}\n" <> format_trailing(prompt, "(#{Enum.at(choices, default)})")
+  end
+  
+  defp ask(prompt, default \\ nil) do
+    case IO.gets(prompt) do
+      :eof ->
+        ""
+      {:error, _reason} ->
+        ""
+      str ->
+        case String.trim_trailing(str) do
+          "" -> default || ""
+          value -> value
+        end
+    end
+  end
+
 end
--- test/ex_prompt_test.exs
+++ test/ex_prompt_test.exs
@@ -6,14 +6,14 @@ defmodule ExPromptTest do
     assert capture_io("", fn ->
              ExPrompt.string("Favorite color?")
              send(self(), :eof)
-           end) == "Favorite color?"
+           end) == "Favorite color? "
 
     assert_received :eof
 
     assert capture_io("", fn ->
              ExPrompt.string("Favorite color?")
              send(self(), {:error, :oops})
-           end) == "Favorite color?"
+           end) == "Favorite color? "
 
     assert_received {:error, :oops}
   end
@@ -22,9 +22,33 @@ defmodule ExPromptTest do
     assert capture_io("blue", fn ->
              ExPrompt.string("Favorite color?")
              send(self(), "blue")
-           end) == "Favorite color?"
+           end) == "Favorite color? "
 
     assert_received "blue"
+  end
+  
+  test ".string/1 keeps the trailing spaces added by the user" do
+    assert capture_io("blue\n", fn ->
+             color = ExPrompt.string("Favorite color?\n\n  ")
+             assert color == "blue"
+           end) == "Favorite color?\n\n  "
+
+  end
+  
+  test ".string/2 returns default value when empty is passed" do
+    default = "red"
+    assert capture_io("\n", fn ->
+             response = ExPrompt.string("Favorite color?", default)
+             assert response == default
+           end) == "Favorite color? (#{default}) "
+  end
+  
+  test ".string/2 keeps the trailing spaces added by the user and returns the default value when an empty value is passed" do
+    default = "red"
+    assert capture_io("\n", fn ->
+             response = ExPrompt.string("Favorite color?\n\n  ", default)
+             assert response == default
+           end) == "Favorite color? (#{default})\n\n  "
   end
 
   test ".string_required/1 keeps asking until we get an answer" do
@@ -32,7 +56,7 @@ defmodule ExPromptTest do
              ExPrompt.string("Favorite color?")
              send(self(), "")
              send(self(), "blue")
-           end) == "Favorite color?"
+           end) == "Favorite color? "
 
     assert_received ""
     assert_received "blue"
@@ -47,7 +71,7 @@ defmodule ExPromptTest do
                response = ExPrompt.confirm("Are you sure?")
                send(self(), answer)
                assert response == true
-             end) == "Are you sure? [Yn] "
+             end) == "Are you sure? [yn] "
 
       assert_received ^answer
     end
@@ -57,10 +81,17 @@ defmodule ExPromptTest do
                response = ExPrompt.confirm("Are you sure?")
                send(self(), answer)
                assert response == false
-             end) == "Are you sure? [Yn] "
+             end) == "Are you sure? [yn] "
 
       assert_received ^answer
     end
+  end
+  
+  test ".confirm/1 keeps the trailing spaces added by the user" do
+    assert capture_io("y\n", fn ->
+      response = ExPrompt.confirm("Are you sure?\n\n  ")
+      assert response == true
+    end) == "Are you sure? [yn]\n\n  "
   end
 
   test ".confirm/1 keeps asking when the answer none of following: 'yes, Yes, YES, y, Y' or 'no, No, NO, n, N'" do
@@ -69,10 +100,40 @@ defmodule ExPromptTest do
              send(self(), "nein")
              send(self(), "no")
              assert answer == false
-           end) == "Are you sure? [Yn] "
+           end) == "Are you sure? [yn] "
 
     assert_received "nein"
     assert_received "no"
+  end
+
+  test ".confirm/2 succeeds when default value is provided" do
+    default_values = [{true, "[Yn]"}, {false, "[yN]"}]
+
+    for {default, options} <- default_values do
+      assert capture_io("\n", fn ->
+               response = ExPrompt.confirm("Are you sure?", default)
+               assert response == default
+             end) == "Are you sure? #{options} "
+
+    end
+  end
+  
+  test ".confirm/2 keeps asking when default value is provided but a non empty value is passed'" do
+    default_values = [{true, "[Yn]"}, {false, "[yN]"}]
+
+    for {default, options} <- default_values do
+      assert capture_io("non_boolean\nnon_empty\n\n", fn ->
+               response = ExPrompt.confirm("Are you sure?", default)
+               assert response == default
+             end) == String.duplicate("Are you sure? #{options} ", 3)
+    end
+  end
+  
+  test ".confirm/2 keeps the trailing spaces added by the user and returns the default value when an empty value is passed" do
+    assert capture_io("\n", fn ->
+      response = ExPrompt.confirm("Are you sure?\n\n  ", true)
+      assert response == true
+    end) == "Are you sure? [Yn]\n\n  "
   end
 
   test ".choose/2 succeeds by list index" do
@@ -92,6 +153,22 @@ defmodule ExPromptTest do
              |> String.trim_trailing("\n")
 
     assert_received "1"
+  end
+  
+  test ".choose/2 keeps the trailing spaces added by the user and succeeds by list index" do
+    assert capture_io("2\n", fn ->
+             idx = ExPrompt.choose("Favorite color?\n\n  ", ~w(red green blue))
+             assert idx == 1
+           end) ==
+             """
+
+               1) red
+               2) green
+               3) blue
+
+             Favorite color?\n\n  
+             """
+             |> String.trim_trailing("\n")
   end
 
   test ".choose/2 succeeds by list value" do
@@ -149,6 +226,44 @@ defmodule ExPromptTest do
              |> String.trim_trailing("\n")
 
     assert_received "none"
+  end
+  
+  test ".choose/3 succeeds with default value" do
+    assert capture_io("\n", fn ->
+             idx = ExPrompt.choose("Favorite color?", ~w(red green blue), 1)
+             assert idx == 1
+           end) ==
+             """
+
+               1) red
+               2) green
+               3) blue
+
+             Favorite color? (green) 
+             """
+             |> String.trim_trailing("\n")
+  end
+  
+  test ".choose/3 keeps the trailing spaces added by the user succeeds with default value" do
+    assert capture_io("\n", fn ->
+             idx = ExPrompt.choose("Favorite color?\n\n  ", ~w(red green blue), 1)
+             assert idx == 1
+           end) ==
+             """
+
+               1) red
+               2) green
+               3) blue
+
+             Favorite color? (green)\n\n  
+             """
+             |> String.trim_trailing("\n")
+  end
+  
+  test ".choose/3 raise exception when default value is out ou bounds" do
+    assert_raise RuntimeError, fn ->
+      ExPrompt.choose("Favorite color?", ~w(red green blue), 4)
+    end
   end
 
   test ".password/2 hides the password by default (when passing true)" do
